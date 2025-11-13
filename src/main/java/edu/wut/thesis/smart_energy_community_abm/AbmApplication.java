bb503f7b755f4b50d253bb478242e417b23dad94
YouRekt <czarnecki.jurko@gmail.com>
2025-11-13 19:41:26 +0100

    spring setup

---
+++ src/main/java/edu/wut/thesis/smart_energy_community_abm/AbmApplication.java
@@ -0,0 +1,13 @@
+package edu.wut.thesis.smart_energy_community_abm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbmApplication.class, args);
+	}
+
+}

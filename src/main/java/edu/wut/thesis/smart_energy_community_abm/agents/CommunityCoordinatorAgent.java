73f2eadae8788aa73749202fd6ca8564005e416a
YouRekt <czarnecki.jurko@gmail.com>
2025-11-19 19:09:42 +0100

    fixed imports2

--- src/main/java/edu/wut/thesis/smart_energy_community_abm/agents/CommunityCoordinatorAgent.java
+++ src/main/java/edu/wut/thesis/smart_energy_community_abm/agents/CommunityCoordinatorAgent.java
@@ -2,18 +2,16 @@ package edu.wut.thesis.smart_energy_community_abm.agents;
 
 import edu.wut.thesis.smart_energy_community_abm.behaviours.agents.CommunityCoordinatorAgent.SimulationTickBehaviour;
 import edu.wut.thesis.smart_energy_community_abm.behaviours.agents.CommunityCoordinatorAgent.StartNewTickBehaviour;
-import edu.wut.thesis.smart_energy_community_abm.util.LogSeverity;
 import jade.core.AID;
-import jade.core.behaviours.TickerBehaviour;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class CommunityCoordinatorAgent extends BaseAgent {
     public static final String AGENT_NAME = "CommunityCoordinatorAgent";
+    public final List<AID> healthyAgents = new ArrayList<>();
     public long tick = 0;
     public short phase = 1;
-    public final List<AID> healthyAgents = new ArrayList<>();
 
     @Override
     protected void setup() {

0ad2a28ca9c3eb3267bff030e09c28814c83b0a9
YouRekt <czarnecki.jurko@gmail.com>
2025-11-19 20:19:32 +0100

    removed the agent container (┬┬﹏┬┬)

--- src/main/java/edu/wut/thesis/smart_energy_community_abm/application/JadeService.java
+++ src/main/java/edu/wut/thesis/smart_energy_community_abm/application/JadeService.java
@@ -14,7 +14,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;

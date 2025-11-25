acc7483f0454c8d90fa3c505f232435c03051480
YouRekt <czarnecki.jurko@gmail.com>
2025-11-25 03:14:09 +0100

    added TODO

--- src/main/java/edu/wut/thesis/smart_energy_community_abm/agents/HouseholdCoordinatorAgent.java
+++ src/main/java/edu/wut/thesis/smart_energy_community_abm/agents/HouseholdCoordinatorAgent.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 public class HouseholdCoordinatorAgent extends BaseAgent {
     public final List<AID> healthyAppliances = new ArrayList<>();
+    //TODO: Remove
     public String name;
     public long tick = 0;
     public Integer applianceCount;

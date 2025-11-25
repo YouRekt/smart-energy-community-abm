830f2c53e89f0b2b43fb1c15b83c7328f1214bd2
YouRekt <czarnecki.jurko@gmail.com>
2025-11-25 22:13:03 +0100

    changed configs to Records

--- src/main/java/edu/wut/thesis/smart_energy_community_abm/domain/BatteryConfig.java
+++ src/main/java/edu/wut/thesis/smart_energy_community_abm/domain/BatteryConfig.java
@@ -3,21 +3,30 @@ package edu.wut.thesis.smart_energy_community_abm.domain;
 import edu.wut.thesis.smart_energy_community_abm.agents.CommunityBatteryAgent;
 import edu.wut.thesis.smart_energy_community_abm.domain.interfaces.AgentConfig;
 
-// TODO: Remove minChargeThreshold -> moved to CommunityCoordinatorAgent
-public record BatteryConfig(Double capacity, Double minChargeThreshold) implements AgentConfig {
+public record BatteryConfig(
+        Double capacity,
+        Double startingCharge,
+        Boolean isPercentage
+) implements AgentConfig {
     public BatteryConfig {
         if (capacity == null || capacity <= 0) {
             throw new IllegalArgumentException("capacity argument cannot be null or less than zero");
         }
 
-        if (minChargeThreshold == null || minChargeThreshold <= 0 || minChargeThreshold > 1.0) {
+        if (startingCharge == null || startingCharge <= 0 || startingCharge > 1.0) {
             throw new IllegalArgumentException("minChargeThreshold argument cannot be null or less than zero");
         }
 
+        if (isPercentage == null || !isPercentage) {
+            throw new IllegalArgumentException("percentage argument cannot be null or less than zero");
+        }
     }
 
     @Override
     public AgentParams getAgentParams() {
-        return new AgentParams(CommunityBatteryAgent.class.getSimpleName(), CommunityBatteryAgent.class, new Object[]{capacity, minChargeThreshold});
+        return new AgentParams(CommunityBatteryAgent.class.getSimpleName(), CommunityBatteryAgent.class, new Object[]{
+                capacity,
+                isPercentage ? startingCharge : startingCharge * capacity,
+        });
     }
 }
--- src/main/java/edu/wut/thesis/smart_energy_community_abm/domain/GreenEnergySourceConfig.java
+++ src/main/java/edu/wut/thesis/smart_energy_community_abm/domain/GreenEnergySourceConfig.java
@@ -3,11 +3,14 @@ package edu.wut.thesis.smart_energy_community_abm.domain;
 import edu.wut.thesis.smart_energy_community_abm.agents.GreenEnergyAgent;
 import edu.wut.thesis.smart_energy_community_abm.domain.interfaces.AgentConfig;
 
-/**
- * @param period Power Stats
- */
-public record GreenEnergySourceConfig(Long period, Double maxOutputPower, Long mu, Double sigma,
-                                      String agentName) implements AgentConfig {
+public record GreenEnergySourceConfig(
+        Long period,
+        Double maxOutputPower,
+        Long peakTick,
+        Double stdDev,
+        Double variation,
+        String agentName
+) implements AgentConfig {
 
     public GreenEnergySourceConfig {
         if (period == null || period <= 0) {
@@ -18,22 +21,33 @@ public record GreenEnergySourceConfig(Long period, Double maxOutputPower, Long m
             throw new IllegalArgumentException("maxOutputPower argument is null or negative");
         }
 
-        if (mu == null || mu <= 0) {
-            throw new IllegalArgumentException("mu argument is null or negative");
+        if (peakTick == null || peakTick <= 0) {
+            throw new IllegalArgumentException("peakTick argument is null or negative");
         }
 
-        if (sigma == null || sigma <= 0) {
-            throw new IllegalArgumentException("sigma argument is null or negative");
+        if (stdDev == null || stdDev <= 0) {
+            throw new IllegalArgumentException("stdDev argument is null or negative");
+        }
+
+        if (variation == null || variation <= 0 || variation > 1) {
+            throw new IllegalArgumentException("variation argument is null or abs() > 1");
         }
 
         if (agentName == null || agentName.isBlank()) {
             throw new IllegalArgumentException("agentName argument is null or blank");
         }
-
     }
 
     @Override
     public AgentParams getAgentParams() {
-        return new AgentParams(agentName, GreenEnergyAgent.class, new Object[]{period, maxOutputPower, mu, sigma});
+        return new AgentParams(agentName,
+                GreenEnergyAgent.class,
+                new Object[]{
+                        period,
+                        maxOutputPower,
+                        peakTick,
+                        stdDev,
+                        variation,
+                });
     }
 }

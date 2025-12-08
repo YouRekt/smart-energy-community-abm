7e04ec7e2c6101df3fa693ca52ae2deac74ddad6
7blak <theblack2233@gmail.com>
2025-12-08 23:17:42 +0100

    initial phase 3 skeleton

--- src/main/java/edu/wut/thesis/smart_energy_community_abm/behaviours/agents/CommunityCoordinatorAgent/Phase3/AskForRequestsBehaviour.java
+++ src/main/java/edu/wut/thesis/smart_energy_community_abm/behaviours/agents/CommunityCoordinatorAgent/Phase3/AskForRequestsBehaviour.java
@@ -2,8 +2,15 @@ package edu.wut.thesis.smart_energy_community_abm.behaviours.agents.CommunityCoo
 
 import edu.wut.thesis.smart_energy_community_abm.agents.CommunityCoordinatorAgent;
 import jade.core.behaviours.OneShotBehaviour;
+import jade.lang.acl.ACLMessage;
 
+import java.util.Date;
+
+// TODO: Create a commmon behaviour so we do not have to repeat this code
 public final class AskForRequestsBehaviour extends OneShotBehaviour {
+    // TODO: Move these strings somewhere or develop a cleaner way of using them
+    public static final String REQUEST_REPLY_BY = "request-reply-by";
+    private static final long REPLY_BY_DELAY = 400;
     private final CommunityCoordinatorAgent agent;
 
     public AskForRequestsBehaviour(CommunityCoordinatorAgent agent) {
@@ -13,6 +20,11 @@ public final class AskForRequestsBehaviour extends OneShotBehaviour {
 
     @Override
     public void action() {
-
+        ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
+        agent.householdAgents.forEach(msg::addReceiver);
+        Date replyBy = new Date(System.currentTimeMillis() + REPLY_BY_DELAY);
+        msg.setReplyByDate(replyBy);
+        agent.send(msg);
+        getDataStore().put(REQUEST_REPLY_BY, replyBy);
     }
 }

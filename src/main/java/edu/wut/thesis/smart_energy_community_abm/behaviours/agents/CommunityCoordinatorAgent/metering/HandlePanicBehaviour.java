e1762d43b874d8bcadc628146838c6b71606c6ae
7blak <theblack2233@gmail.com>
2026-01-15 22:44:35 +0100

    fix dataStores not working properly

--- src/main/java/edu/wut/thesis/smart_energy_community_abm/behaviours/agents/CommunityCoordinatorAgent/metering/HandlePanicBehaviour.java
+++ src/main/java/edu/wut/thesis/smart_energy_community_abm/behaviours/agents/CommunityCoordinatorAgent/metering/HandlePanicBehaviour.java
@@ -3,16 +3,19 @@ package edu.wut.thesis.smart_energy_community_abm.behaviours.agents.CommunityCoo
 import edu.wut.thesis.smart_energy_community_abm.agents.CommunityCoordinatorAgent;
 import edu.wut.thesis.smart_energy_community_abm.behaviours.agents.CommunityCoordinatorAgent.metering.Panic.CollectAndFinalizePostponeResponsesBehaviour;
 import edu.wut.thesis.smart_energy_community_abm.behaviours.agents.CommunityCoordinatorAgent.metering.Panic.CollectPostponeResponsesBehaviour;
-import edu.wut.thesis.smart_energy_community_abm.behaviours.agents.CommunityCoordinatorAgent.metering.Panic.PrepareAndSendPostponeCFPBehaviour;
 import edu.wut.thesis.smart_energy_community_abm.behaviours.agents.CommunityCoordinatorAgent.metering.Panic.ProcessPostponeResponsesBehaviour;
+import edu.wut.thesis.smart_energy_community_abm.behaviours.agents.CommunityCoordinatorAgent.metering.Panic.SendPostponeCFPBehaviour;
 import edu.wut.thesis.smart_energy_community_abm.behaviours.base.PhaseBehaviour;
 import edu.wut.thesis.smart_energy_community_abm.domain.constants.LogSeverity;
 import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.DataStore;
 import jade.core.behaviours.OneShotBehaviour;
 
 public final class HandlePanicBehaviour extends PhaseBehaviour {
-    public HandlePanicBehaviour(CommunityCoordinatorAgent agent) {
+    public HandlePanicBehaviour(CommunityCoordinatorAgent agent, DataStore dataStore) {
         super(agent);
+        setDataStore(dataStore);
+
         setupSubBehaviours(new Behaviour[]{
                 new OneShotBehaviour(agent) {
                     public void action() {

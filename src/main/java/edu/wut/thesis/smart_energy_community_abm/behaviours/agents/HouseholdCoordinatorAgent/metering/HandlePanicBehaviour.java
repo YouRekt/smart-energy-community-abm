f05e88b7f437a2be45e587775ac5c49f99096961
7blak <theblack2233@gmail.com>
2026-01-16 01:47:04 +0100

    fix to wait for ApplianceAgents in phase 3

--- src/main/java/edu/wut/thesis/smart_energy_community_abm/behaviours/agents/HouseholdCoordinatorAgent/metering/HandlePanicBehaviour.java
+++ src/main/java/edu/wut/thesis/smart_energy_community_abm/behaviours/agents/HouseholdCoordinatorAgent/metering/HandlePanicBehaviour.java
@@ -5,27 +5,43 @@ import edu.wut.thesis.smart_energy_community_abm.behaviours.agents.HouseholdCoor
 import edu.wut.thesis.smart_energy_community_abm.behaviours.agents.HouseholdCoordinatorAgent.metering.Panic.CollectProposalResultBehaviour;
 import edu.wut.thesis.smart_energy_community_abm.behaviours.agents.HouseholdCoordinatorAgent.metering.Panic.HandlePostponeRepliesBehaviour;
 import edu.wut.thesis.smart_energy_community_abm.behaviours.agents.HouseholdCoordinatorAgent.metering.Panic.SendPostponeRequestsBehaviour;
-import edu.wut.thesis.smart_energy_community_abm.behaviours.base.PhaseBehaviour;
+import edu.wut.thesis.smart_energy_community_abm.behaviours.base.BaseFSMBehaviour;
+import edu.wut.thesis.smart_energy_community_abm.behaviours.base.LogBehaviour;
 import edu.wut.thesis.smart_energy_community_abm.domain.constants.LogSeverity;
 import jade.core.behaviours.Behaviour;
 import jade.core.behaviours.DataStore;
 import jade.core.behaviours.OneShotBehaviour;
+import jade.lang.acl.ACLMessage;
 
-public final class HandlePanicBehaviour extends PhaseBehaviour {
+public final class HandlePanicBehaviour extends BaseFSMBehaviour {
+    private static final String LOG = "log";
+    private static final String SEND_POSTPONE_REQUESTS = "send-postpone-requests";
+    private static final String COLLECT_POSTPONE_REPLIES = "collect-postpone-replies";
+    private static final String HANDLE_POSTPONE_REPLIES = "handle-postpone-replies";
+    private static final String COLLECT_PROPOSAL_RESULT = "collect-proposal-result";
+    private static final String EXIT = "exit";
+
     public HandlePanicBehaviour(HouseholdCoordinatorAgent agent, DataStore dataStore) {
         super(agent);
         setDataStore(dataStore);
 
-        setupSubBehaviours(new Behaviour[]{
-                new OneShotBehaviour(agent) {
-                    public void action() {
-                        agent.log(String.format("--- Handle Panic: Tick %d ---", agent.tick), LogSeverity.INFO, this);
-                    }
-                },
-                new SendPostponeRequestsBehaviour(agent),
-                new CollectPostponeRepliesBehaviour(agent),
-                new HandlePostponeRepliesBehaviour(agent),
-                new CollectProposalResultBehaviour(agent)
-        });
+        registerFirstState(new LogBehaviour(agent, String.format("--- Handle Panic: Tick %d ---", agent.tick), LogSeverity.INFO), LOG);
+        registerState(new SendPostponeRequestsBehaviour(agent), SEND_POSTPONE_REQUESTS);
+        registerState(new CollectPostponeRepliesBehaviour(agent), COLLECT_POSTPONE_REPLIES);
+        registerState(new HandlePostponeRepliesBehaviour(agent), HANDLE_POSTPONE_REPLIES);
+        registerState(new CollectProposalResultBehaviour(agent), COLLECT_PROPOSAL_RESULT);
+        registerLastState(new OneShotBehaviour() {
+            @Override
+            public void action() {
+
+            }
+        }, EXIT);
+
+        registerDefaultTransition(LOG, SEND_POSTPONE_REQUESTS);
+        registerDefaultTransition(SEND_POSTPONE_REQUESTS, COLLECT_POSTPONE_REPLIES);
+        registerDefaultTransition(COLLECT_POSTPONE_REPLIES, HANDLE_POSTPONE_REPLIES);
+        registerTransition(HANDLE_POSTPONE_REPLIES, EXIT, ACLMessage.REFUSE);
+        registerTransition(HANDLE_POSTPONE_REPLIES, COLLECT_PROPOSAL_RESULT, ACLMessage.PROPOSE);
+        registerDefaultTransition(COLLECT_PROPOSAL_RESULT, EXIT);
     }
 }

7977031900ee00e1700150ab054fc5e67e32d562
7blak <theblack2233@gmail.com>
2026-01-22 23:32:12 +0100

    change negotiations to use shouldTriggerPanic

--- src/main/java/edu/wut/thesis/smart_energy_community_abm/domain/strategy/NaiveStrategy.java
+++ src/main/java/edu/wut/thesis/smart_energy_community_abm/domain/strategy/NaiveStrategy.java
@@ -1,7 +1,5 @@
 package edu.wut.thesis.smart_energy_community_abm.domain.strategy;
 
-import edu.wut.thesis.smart_energy_community_abm.domain.PanicContext;
-
 public final class NaiveStrategy implements NegotiationStrategy {
     @Override
     public String getName() {
@@ -10,21 +8,21 @@ public final class NaiveStrategy implements NegotiationStrategy {
 
     @Override
     public double computeNegotiationPriority(double greenScore, double cooperationScore, long firstTaskTick, long requestSpan) {
-        // Naive Approach: Return constant 0.0.
-        // This relies on the system's natural processing order (First Come First Serve)
-        // and performs no calculations based on agent scores or task details.
         return 0.0;
     }
 
     @Override
     public double computePostponementPriority(double greenScore, double cooperationScore, double energyToFree) {
-        // Naive Approach: No calculation.
         return 0.0;
     }
 
     @Override
-    public boolean shouldTriggerPanic(PanicContext ctx) {
-        // Always return false, even if there is a shortage (never postpone tasks).
+    public double computeGenericPriority(double greenScore, double cooperationScore) {
+        return 0.0;
+    }
+
+    @Override
+    public boolean shouldTriggerPanic(double shortfall, double batteryCharge, double batteryCapacity) {
         return false;
     }
 }
